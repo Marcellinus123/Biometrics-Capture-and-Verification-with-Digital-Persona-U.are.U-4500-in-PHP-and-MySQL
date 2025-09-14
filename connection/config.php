94a171577fac3089596ec36cef8a9a1e0e0f4f71
Atampugre Marcellinus <65893278+Marcellinus123@users.noreply.github.com>
2025-09-14 16:26:13 +0000

    Add files via upload

--- connection/config.php
+++ connection/config.php
@@ -1,1 +1,14 @@
+<?php
+// config.php
+$host = 'localhost';
+$dbname = 'bioattend_db_system';
+$username = 'root'; 
+$password = ''; 
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
